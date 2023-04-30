1848ec3827f21b71cb168b2e77dfbd8f460acd37
Gordon-Magill <gordon.magill@gmail.com>
2023-04-30 01:44:57 -0700

    Added basic API route

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          Hello! This is my page that I am making through the Next.JS tutorial!
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
